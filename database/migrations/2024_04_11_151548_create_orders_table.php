ea440c976185b3bc3bb6efa3eba9ff0c45369f54
youstina <yostinaanton1@gmail.com>
2024-04-11 08:34:12 -0700

    models of orders

---
+++ database/migrations/2024_04_11_151548_create_orders_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use App\Models\Address;
+use App\Models\Coupon;
+use App\Models\Phone;
+use App\Models\User;
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('orders', function (Blueprint $table) {
+            $table->id();
+            $table->foreignId(User::class)->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId(Address::class)->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId(Coupon::class)->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->foreignId(Phone::class)->constrained()->onUpdate('cascade')->onDelete('cascade');
+            $table->string('payment');
+            $table->string('status');
+            $table->float('total_price');
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('orders');
+    }
+};
